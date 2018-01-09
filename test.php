76701c3396d9930f72c2c3381ba97a00773a9138
yichun668 <35246412+yichun668@users.noreply.github.com>
2018-01-09 03:48:48 +0000

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 echo "hello"; 
 $json_str=file_get_contents('php://input');
 $json_obj=json_decode($json_str);
-$myfile=fopen("log.txt","w+") or die ("unable to openfile.");
-fwrite("$myfile,"\xEF\xBB\xBf",$json_str);
+$myfile=fopen("log.txt","w+") or die ("unable to open file.");
+fwrite("$myfile,"\xEF\xBB\xBf".$json_str);
 fclose($myfile);
 ?> 
